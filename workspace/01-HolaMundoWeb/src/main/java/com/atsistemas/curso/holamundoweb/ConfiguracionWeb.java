8c0bffd57702a29010ae76bfe8caf9a71c778e9a
Victor Herrero Cazurro <victorherrerocazurro@gmail.com>
2019-05-17 15:53:31 +0200

    WEB-MVC: View Controller y Resource Handler

--- workspace/01-HolaMundoWeb/src/main/java/com/atsistemas/curso/holamundoweb/ConfiguracionWeb.java
+++ workspace/01-HolaMundoWeb/src/main/java/com/atsistemas/curso/holamundoweb/ConfiguracionWeb.java
@@ -1,10 +1,16 @@
 package com.atsistemas.curso.holamundoweb;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ConfiguracionWeb implements WebMvcConfigurer{
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("HolaMundo/index");
+	}
 
 	
 	
